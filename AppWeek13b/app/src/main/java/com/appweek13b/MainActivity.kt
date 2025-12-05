d84d331e757041b654aec7f48fe7092d24af3595
deepblue <deepblue@inha.ac.kr>
2025-12-05 12:15:23 +0900

    week13 : Basic Syntax Lab

--- AppWeek13b/app/src/main/java/com/appweek13b/MainActivity.kt
+++ AppWeek13b/app/src/main/java/com/appweek13b/MainActivity.kt
@@ -4,44 +4,49 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.appweek13b.ui.theme.AppWeek13bTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            AppWeek13bTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            LazyColumn(
+                modifier = Modifier
+                    .background(color = Color.Red)
+                    .fillMaxWidth()
+            ) {
+                items(90){ index ->
+                    Text(text = "코틀린 ${index + 1}")
                 }
             }
+
+//            val scrollState = rememberScrollState()
+//
+//            Column(
+//                modifier = Modifier
+//                    .background(color = Color.Blue)
+//                    .fillMaxWidth()
+//                    .verticalScroll(scrollState)
+//            ){
+//                for(i in 1..90){
+//                    Text("코틀린 $i")
+//                }
+//            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppWeek13bTheme {
-        Greeting("Android")
     }
 }
