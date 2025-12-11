ec9fcd84e7a6fadb22022545a1427fe56eaf1c94
voidgogo <2021038@daelim.ac.kr>
2025-12-11 23:14:42 +0900

    week14 : Basic Syntax Lab

--- AppWeek14a/app/build.gradle.kts
+++ AppWeek14a/app/build.gradle.kts
@@ -50,6 +50,14 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Retrofit
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
